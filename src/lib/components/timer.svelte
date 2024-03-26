a7b43ea74fe2e7e02d0655d8ff4a81e567717368
bensch5 <benjaminschroeder46@gmail.com>
2024-03-26 17:23:02 +0100

    add timer component

---
+++ src/lib/components/timer.svelte
@@ -0,0 +1,36 @@
+<script>
+
+    import { Button } from "$lib/components/ui/button";
+
+    let time = 5;
+
+    let countdown;
+
+    function convertTime(x) {
+      let minutes = Math.floor(x/60);
+      let seconds = x % 60;
+      if (seconds < 10) {
+        return "" + minutes + " : 0" + seconds
+      } else {
+        return "" + minutes + " : " + seconds
+      }
+      
+    }
+
+    function startCountdown() {
+      countdown = setInterval(update, 1000);
+    }
+
+    function update() {
+      if (time > 0) {
+        time -= 1;
+      } else {
+        clearInterval(countdown);
+        alert("time's up!")
+      }
+    }
+
+</script>
+<Button on:click={()=>startCountdown()}>Start</Button>
+{convertTime(time)}
+<Button on:click={()=>clearInterval(countdown)}>Stop</Button>
